e8ff4bfd28731d83ba22368a8f25e2f2478bbf61
ParthaMalakar <parthamalakar4321@gmail.com>
2023-09-15 14:03:10 +0600

    Inter front family added and title part done

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        
+            <h1 className="font-bold text-3xl text-center pt-8">Course Registration</h1>
+        
+    );
+};
+
+export default Header;
